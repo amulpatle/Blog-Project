08dd30cf8715f44f633b5bb8b3e825c747e5ac57
amul patle <amulpatle3@gmail.com>
2024-03-31 23:49:00 +0530

    url setup complite

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
-urlspatter = [
-    path(""),
-    path("posts"),
-    path("posts/<slug:slug>"),#/posts/my-first-post
+from . import views
+urlpatterns = [
+    path("",views.starting_page,name="starting-page"),
+    path("posts",views.posts,name="posts-page"),
+    path("posts/<slug:slug>",views.post_details,name="post-detail-page"),#/posts/my-first-post
 ]
