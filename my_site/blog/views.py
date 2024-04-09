d866ae600406f3ea5635a3e6267002777ecdfe6d
amul patle <amulpatle3@gmail.com>
2024-04-09 14:10:31 +0530

    add tags to posts

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -27,4 +27,5 @@ def post_details(request,slug):
     identified_post =  get_object_or_404(Post,slug=slug)
     return render(request,"blog/post-detail.html",{
         "post":identified_post,
+        "Post_tags":identified_post.tags.all(),
     })  
