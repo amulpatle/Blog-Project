1869930dbf7624a5716c5f1889ecb7b248bfa699
amul patle <amulpatle3@gmail.com>
2024-04-14 02:01:50 +0530

    error:-i didn't upload mountain.png img

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,31 +1,59 @@
+# from typing import Any
+# from django.db.models.query import QuerySet
 from django.shortcuts import render,get_object_or_404
+from django.views.generic import ListView,DetailView
+
+
 from datetime import date
 from .models import Post
 # Create your views here.
-all_posts = [
+
+
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
     
-]
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+
+# def starting_page(request):
+#     latest_posts = Post.objects.all().order_by("-date")[:3]
+#     return render(request,"blog/index.html",{
+#         "posts":latest_posts
+#     })
+
+class AllPostView(ListView):
+    template_name = "blog/all-post.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
+    
+
+# def posts(request):
+#     all_posts = Post.objects.all().order_by("-date")
+#     return render(request,"blog/all-post.html",{
+#         "all_posts": all_posts
+#     })
 
 
-def get_date(post):
-    return post['date']
 
 
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3]
-    return render(request,"blog/index.html",{
-        "posts":latest_posts
-    })
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request,"blog/all-post.html",{
-        "all_posts":all_posts,
-    })
-
+# class SinglePostView(DetailView):
+#     template_name = "blog/post-detail.html"
+#     model = Post
+    
+#     def get_context_data(self, **kwargs): 
+#         context =  super().get_context_data(**kwargs)
+#         context["post_tags"] = self.object.tags.all()
+#         return context
 def post_details(request,slug):
     identified_post =  get_object_or_404(Post,slug=slug)
     return render(request,"blog/post-detail.html",{
         "post":identified_post,
-        "Post_tags":identified_post.tags.all(),
+        "Post_tags":identified_post.tags.all()
     })  
