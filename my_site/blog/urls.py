755c6ee4e140c5f13d8b66e9c386046776950268
amul patle <amulpatle3@gmail.com>
2024-04-14 10:20:15 +0530

    add css & comment section

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns = [
     path("",views.StartingPageView.as_view(),name="starting-page"),
     path("posts",views.AllPostView.as_view(),name="posts-page"),
-    path("posts/<slug:slug>",views.post_details,name="post-detail-page"),#/posts/my-first-post
+    path("posts/<slug:slug>",views.SinglePostView.as_view(),name="post-detail-page"),#/posts/my-first-post
 ]
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -2,8 +2,10 @@
 # from django.db.models.query import QuerySet
 from django.shortcuts import render,get_object_or_404
 from django.views.generic import ListView,DetailView
-
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import View
+from .forms import CommentForm
 from datetime import date
 from .models import Post
 # Create your views here.
@@ -40,6 +42,47 @@ class AllPostView(ListView):
 #     })
 
 
+class SinglePostView(View):
+    def is_stored_post(self, request, post_id):
+        stored_posts = request.session.get("stored_posts")
+        if stored_posts is not None:
+          is_saved_for_later = post_id in stored_posts
+        else:
+          is_saved_for_later = False
+
+        return is_saved_for_later
+
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id)
+        }
+        return render(request, "blog/post-detail.html", context)
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form,
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id)
+        }
+        return render(request, "blog/post-detail.html", context)
 
 
 
